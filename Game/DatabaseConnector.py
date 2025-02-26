549d781a69e678d41b05920374354484cfd38735
Mila <turpeinenmila6@gmail.com>
2025-02-27 01:29:11 +0200

    Created a script for database connection
    DatabaseConnector script should be imported into other scripts that need the database connection

---
+++ Game/DatabaseConnector.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+#This script creates the connection to the database and is to be
+#imported to other scripts for database use
+
+#Establishes a connection with the database
+def connect_database():
+    connection = None
+    connection = mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        database='flight_game',
+        user='root',
+        password='KostiKotitonttu',
+        autocommit=True,
+        collation="utf8mb4_general_ci"
+        )
+    return connection
+
+#Creates an object that has the connection
+connection = connect_database()
+
+#Function to execute the selected queries
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    print("Query successful")
+    return result
+    cursor.close()
--- Game/Score.py
+++ Game/Score.py
@@ -1,1 +1,7 @@
-print("Hello world!")
+import DatabaseConnector
+
+#Query holds the desired SQL query
+query = "SELECT * FROM airport"
+
+#Queries will be executed like this via DatabaseConnector
+print(DatabaseConnector.execute_query(DatabaseConnector.connection, query))
